f1b3c0c007d8f5a3fd4ae34d03939b9384aa5cd4
usysrc <usysrc@usysrc.dev>
2024-03-09 23:17:48 +0100

    feat: :sparkles: new example patch

--- main.lua
+++ main.lua
@@ -1,16 +1,25 @@
-lick = require "lick"
+-- this file contains an example of how to use the livecoding environment
+local lick = require "lick"
 lick.reset = true
+lick.debug = true
+local lg = love.graphics
+local sin, cos, pi = math.sin, math.cos, math.pi
 
-function love.load()
-    circle = {}
-    circle.x = 1
-end
-
+local time = 0
 function love.update(dt)
-    circle.x = circle.x + dt*5
+    time = time + dt
 end
 
 function love.draw(dt)
-    love.graphics.setColor(1,1,0)
-    love.graphics.circle("fill", 400+100*math.sin(circle.x), 300, 16,16)
+    lg.setBlendMode("alpha")
+    lg.translate(lg.getWidth() / 2, lg.getHeight() / 2)
+    lg.rotate(1 * pi * cos(time / 10))
+    lg.scale(1 + cos(time / 2.5) * 0.2, 1 + sin(time / 5) * 0.2)
+    for i = 1, 10 do
+        lg.setColor(0.75 + cos(pi / i + time) * 0.25, 0, sin(pi / i * time), 0.91)
+        lg.circle("fill",
+            400 * sin(i / 3 * pi + time / 10),
+            300 * sin(i / 4 * pi + time / 10),
+            200 * cos(i / 4 * pi + time / 5), 128)
+    end
 end
