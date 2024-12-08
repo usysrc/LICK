2cc64ce5ead375ef7afb1404efbcb35bf04db6b6
usysrc <usysrc@usysrc.dev>
2024-12-08 21:47:47 +0100

    feat: :sparkles: 11.5 compatible love.run

--- lick.lua
+++ lick.lua
@@ -170,41 +170,34 @@ function love.run()
 
     local dt = 0
 
-    -- Main loop time.
-    while true do
-        -- Process events.
+    return function()
         if love.event then
             love.event.pump()
-            for e, a, b, c, d in love.event.poll() do
-                if e == "quit" then
+            for name, a, b, c, d, e, f in love.event.poll() do
+                if name == "quit" then
                     if not love.quit or not love.quit() then
-                        if love.audio then
-                            love.audio.stop()
-                        end
-                        return
+                        return a or 0
                     end
                 end
-
-                love.handlers[e](a, b, c, d)
+                love.handlers[name](a, b, c, d, e, f)
             end
         end
 
         -- Update dt, as we'll be passing it to update
         if love.timer then
-            love.timer.step()
-            dt = love.timer.getDelta()
+            dt = love.timer.step()
         end
 
         -- Call update and draw
         if update then update(dt) end -- will pass 0 if love.timer is disabled
-        if love.graphics then
+        if love.graphics and love.graphics.isActive() then
             love.graphics.origin()
             love.graphics.clear(love.graphics.getBackgroundColor())
             if draw then draw() end
+            love.graphics.present()
         end
 
         if love.timer then love.timer.sleep(lick.sleepTime) end
-        if love.graphics then love.graphics.present() end
     end
 end
 
